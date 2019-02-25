785c9cc31b44b9a53bb8d6ccc25cfda8a5ecbf53
darnelho1 <darnelho1@gmail.com>
2019-02-25 10:40:45 -0800

    [added docusign sdk]

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 var port = 5050;
 app.get('/', function(req, res) {
-  res.send('Hello World')
+  res.render('index')
 })
 
 app.listen(port, function(){
   console.log('App Listening on port: ' + port)
+})
+
+app.post('/', function(req, res){
+  res.render('index');
+  console.log("I've been clicked")
 })
