bf2b47fa9e14ce922577461c764321c5568a5ac7
darnelho1 <darnelho1@gmail.com>
2019-02-26 10:22:52 -0800

    [added log messages]

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/e5', function (req, res) {
     //console.log(oAuthToken.accessToken);
     apiClient.getUserInfo(oAuthToken.accessToken, function (err, userInfo) {
       oAuthAccessToken = oAuthToken.accessToken;
-      console.log(oAuthToken);
+      console.log("the oAuthToken is :" + JSON.stringify(oAuthToken));
       //console.log(oAuthAccessToken);
       //apiClient.setBasePath(basePath);
       // var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/++[++^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
@@ -68,9 +68,9 @@ app.get('/e5', function (req, res) {
       //console.log(encodedString);
       apiClient.addDefaultHeader('Authorization', 'Bearer ' + oAuthToken.accessToken);
       apiClient.setBasePath(userInfo.accounts[0].baseUri + '/restapi')
-      console.log(userInfo.accounts[0].baserUri)
-      console.log(userInfo.accounts[0].accountId);
-      //console.log(basePath);
+      //console.log(userInfo.accounts[0].baserUri)
+      //console.log(userInfo.accounts[0].accountId);
+      console.log("the deafult header is: " + JSON.stringify( apiClient.defaultHeaders));
       //console.log(apiclient.basePath);
       res.render('index')
       //createEnvelope(userInfo.accounts[0].accountId, oAuthToken.accessToken);
