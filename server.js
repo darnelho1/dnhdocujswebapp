ef7de9e071a18a48ee605f24cc406f4b6027524c
darnelho1 <darnelho1@gmail.com>
2019-02-26 11:30:41 -0800

    [authentication working]

--- server.js
+++ server.js
@@ -55,20 +55,11 @@ app.get('/', function (req, res) {
 app.get('/e5', function (req, res) {
 
   apiClient.generateAccessToken(clientID, clientSecret, req.query.code, function (err, oAuthToken) {
-    //console.log(oAuthToken.accessToken);
+    console.log(oAuthToken.accessToken);
     apiClient.getUserInfo(oAuthToken.accessToken, function (err, userInfo) {
       oAuthAccessToken = oAuthToken.accessToken;
       //console.log(oAuthToken);
-      //console.log(oAuthAccessToken);
-      //apiClient.setBasePath(basePath);
-      // var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/++[++^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-      //
-      // // Define the string
-      // var string = clientID + ':' + clientSecret;
-      //
-      // // Encode the String
-      // encodedString = Base64.encode(string);
-      //console.log(encodedString);
+      console.log(oAuthAccessToken);
       apiClient.addDefaultHeader('Authorization', 'Bearer ' + oAuthToken.accessToken);
       apiClient.setBasePath(userInfo.accounts[0].baseUri + '/restapi')
       console.log(userInfo.accounts[0].baserUri)
@@ -87,13 +78,17 @@ app.listen(port, function(){
 
 
 app.post('/a', function listEnvelopesController(req, res){
-  console.log(oAuthAccessToken)
-  //apiClient.setBasePath(basePath);
-    // apiClient.addDefaultHeader('Authorization ' + 'Bearer ', 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQkAAAABAAUABwAASfo5ipvWSAgAAIkdSM2b1kgCAJ8giZesq0hAr93A-qwGx1gVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MAAAUbpMfZvWSDcAYEcIb3TtBU6HTBBHVB7pfg.mr1LXIQKuj79-eY7MwRBdBKd6oiehj3ZaObXtqlZbMree5pDUJLDPZoyFxPIHAXUoxfMCp6IbtEmphzKquMAGHC4vYDNl0E_QlQOpfKD-GF5fQcMNNTj1m4-0r6PwNpI2zfkPpQ6-5n3jumY7M4Y7mMA2G5H7lo-DiyqJXMG8sXTlHU_Ckd9AUOkEZJJ9NKrOj8ibkIW2b2qGDRI2h2Z5f7ZtKuefAYhSofq0ipECY8kq_yt-jIIhTvlj0qexwbZj_66TKJ0biqZWQtA7v9AT4Lk1k9EHMQFbut3Q8X3SrMkvzXe_8lR_ITzwXK3lU4nC0jIjywzJBuKADA4vcbcUA');
+  //console.log(oAuthAccessToken)
+    //apiClient.setBasePath(basePath);
+    //apiClient.addDefaultHeader('Authorization' , 'Bearer ' + oAuthAccessToken);
+    docusign.Configuration.default.setDefaultApiClient(apiClient);
     var envelopesApi = new docusign.EnvelopesApi();
-    envelopesApi.listRecipients("5c4b10ee-19de-4395-ba74-b99a6727b40f", "698db573-cda0-402c-9730-105fb68f1fef", null, function (error, recips, response) {
+    envelopesApi.listRecipients('8006154', "6a2d9802-8976-4840-b434-62c6b5ae194d", null, function (error, recips, response) {
       if (error) {
         console.log('Error: ' + error);
+        console.log(JSON.stringify(apiClient.defaultHeaders));
+        console.log(apiClient.baseUri);
+        console.log(response);
         return;
       }
       if (recips) {
@@ -104,9 +99,6 @@ app.post('/a', function listEnvelopesController(req, res){
     });
     console.log("I've been clicked")
   })
-
-//  apiClient.setBasePath(basePath);
-//  apiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
 
 
 
