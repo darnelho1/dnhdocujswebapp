a8338cb6bb2c58caf978c9973dedb47d70a9697c
darnelho1 <darnelho1@gmail.com>
2019-02-26 20:53:46 -0800

    [all clean]

--- server.js
+++ server.js
@@ -15,25 +15,19 @@ const app = express()
     , hostUrl = 'http://'+ host + ':' + port
     , clientID = '47f1113d-6380-4886-b0a6-cba02f960879'
     , clientSecret = 'e3122117-0328-4c3d-8381-b7e35d8404b9'
-    , signerEmail = 'darnelho1@gmail.com'
-    , signerName = 'Darnell Holder'
-    , baseUriSuffix = '/restapi'
-    , testDocumentPath = '../demo_documents/test.pdf'
-    , test2DocumentPath = '../demo_documents/battle_plan.docx'
     , redirectUri ="http://localhost:5050/e5"
     , basePath = 'https://demo.docusign.net/restapi'
     , apiClient = new docusign.ApiClient()
-    , scopes = [apiClient.OAuth.Scope.SIGNATURE];
+    , scopes = [apiClient.OAuth.Scope.SIGNATURE]
+    , responseTypeCode = apiClient.OAuth.ResponseType.CODE
+    , responseTypeToken = apiClient.OAuth.ResponseType.TOKEN;
 
-const responseTypeCode = apiClient.OAuth.ResponseType.CODE; // Response type of code, to be used for the Auth code grant
-const responseTypeToken = apiClient.OAuth.ResponseType.TOKEN; // Response type of token, to be used for implicit grant
+var oAuthAccessToken
+  , envelopeId
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "/public")));
-
-
-var oAuthAccessToken
-var envelopeId
 
 apiClient.setBasePath(basePath);
 
@@ -55,7 +49,6 @@ app.listen(port, function(){
   console.log('App Listening on port: ' + port)
 })
 
-
 app.get('/a', function listRecipientsController(req, res){
     docusign.Configuration.default.setDefaultApiClient(apiClient);
     var envelopesApi = new docusign.EnvelopesApi();
@@ -66,7 +59,7 @@ app.get('/a', function listRecipientsController(req, res){
       }
       if (recips) {
         console.log('Recipients: ' + JSON.stringify(recips));
-        res.send(recips);
+        res.send("<pre>" + JSON.stringify(recips) + "</pre>" + '<a href="/"> Return Home </a>');
       }
       return recips;
     });
@@ -77,7 +70,7 @@ app.get('/a', function listRecipientsController(req, res){
 app.get('/b',function getUserInfoController(req,res){
   console.log(oAuthAccessToken);
   apiClient.getUserInfo(oAuthAccessToken, function(err, userInfo){
-    res.send(userInfo)
+    res.send("<pre>" + JSON.stringify(userInfo) + "</pre>" + '<a href="/"> Return Home </a>')
   })
 
 })
@@ -92,7 +85,7 @@ app.get('/c', function listenvelopesDocumentsController(req,res){
       return;
     }
     if (docs) {
-      res.send(docs)
+      res.send("<pre>" + JSON.stringify(docs) + "</pre>" + '<a href="/"> Return Home </a>')
     }
 });
 
@@ -115,7 +108,7 @@ app.get('/d',function downloadEnvelopeDocumentsController(req,res){
         fs.writeFile(tempFile, new Buffer(document, 'binary'), function (err) {
           if (err) console.log('Error: ' + err);
         });
-        res.send('Document ' + '1' + ' from envelope ' + envelopeId + ' has been downloaded to:\n' + tempFile);
+        res.send('Document ' + '1' + ' from envelope ' + envelopeId + ' has been downloaded to:\n' + tempFile + '<a href="/"> Return Home </a>');
       } catch (ex) {
         res.send('Exception: ' + ex);
       }
@@ -130,12 +123,10 @@ fileStream = process.argv[2];
 pdfBytes = fs.readFileSync(path.resolve(__dirname, 'test.pdf'));
 pdfBase64 = pdfBytes.toString('base64');
 
-
 let envDef = new docusign.EnvelopeDefinition();
 
-//Set the Email Subject line and email message
-envDef.emailSubject = 'Please sign this document sent from Node SDK';
-envDef.emailBlurb = 'Please sign this document sent from the DocuSign Node.JS SDK.'
+envDef.emailSubject = 'Please sign this document sent from Darnell';
+envDef.emailBlurb = 'Please sign this document sent from Darnell using the Node.JS SDK.'
 
 //Read the file from the document and convert it to a Base64String
 let doc = new docusign.Document();
@@ -144,55 +135,43 @@ doc.fileExtension = 'pdf';
 doc.name = 'Node Doc Send Sample';
 doc.documentId = '1';
 
-//Push the doc to the documents array.
 let docs = [];
 docs.push(doc);
 envDef.documents = docs;
 
-//Create the signer with the previously provided name / email address
 let signer = new docusign.Signer();
 signer.name = 'Darnell Holder';
 signer.email = 'holderdarnell2@gmail.com';
 signer.routingOrder = '1';
 signer.recipientId = '1';
-signer.clientUserId = '123'; //ClientUserId specifies that a recipient is captive. It ties to a generic DocuSign account and cannot be referenced without generating a recipient token.
+signer.clientUserId = '123';
 
-//Create a tabs object and a signHere tab to be placed on the envelope
 let tabs = new docusign.Tabs();
 
 let signHere = new docusign.SignHere();
 signHere.documentId = '1';
-signHere.pageNumber = '1';
+signHere.pageNumber = '3';
 signHere.recipientId = '1';
 signHere.tabLabel = 'SignHereTab';
 signHere.xPosition = '50';
-signHere.yPosition = '50';
+signHere.yPosition = '550';
 
-//Create the array for SignHere tabs, then add it to the general tab array
 signHereTabArray = [];
 signHereTabArray.push(signHere);
 
 tabs.signHereTabs = signHereTabArray;
 
-//Then set the recipient, named signer, tabs to the previously created tab array
 signer.tabs = tabs;
 
-//Add the signer to the signers array
 let signers = [];
 signers.push(signer);
 
-//Envelope status for drafts is created, set to sent if wanting to send the envelope right away
 envDef.status = 'sent';
 
-//Create the general recipients object, then set the signers to the signer array just created
 let recipients = new docusign.Recipients();
 recipients.signers = signers;
 
-//Then add the recipients object to the enevelope definitions
 envDef.recipients = recipients;
-
-// *** End envelope creation ***
-
 
 //Send the envelope
 let envelopesApi = new docusign.EnvelopesApi();
@@ -201,7 +180,6 @@ envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, functio
   if (err) {
     return res.send('Error while creating a DocuSign envelope:' + err);
   }
-  //Set envelopeId the envelopeId that was just created
   envelopeId = envelopeSummary.envelopeId;
   console.log(envelopeId);
 
@@ -214,7 +192,6 @@ envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, functio
   recipientViewRequest.userName = 'Darnell Holder';
   recipientViewRequest.email = 'holderdarnell2@gmail.com';
 
-  //Create the variable used to handle the response
   recipientViewResults = docusign.ViewLinkRequest();
 
   //Make the request for a recipient view
